f96baeddce4c2cd7538db2360b147aec9d64e250
yellakrishna <yellakrishna7675@gmail.com>
2025-01-03 22:46:26 +0530

    change vercel frontend

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin:"https://blog-frontend-yella.netlify.app",
+    origin:"https://blog-frontend-chi-henna.vercel.app",
     credentials:true
 }))
 
